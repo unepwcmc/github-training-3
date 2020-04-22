703f623f964ae389a7ff2c02d069edc614804602
Will Kocur <will.kocur@unep-wcmc.org>
2020-04-22 09:06:36 +0100

    make bad edits with bad commit message

--- python/merge_python.py
+++ python/merge_python.py
@@ -8,14 +8,15 @@ import pandas as pd
 df1 = pd.DataFrame({'lkey': ['ARG','FRA', 'GBR', 'VNM'],
                     'value': [100, 200, 300, 500]})
 df2 = pd.DataFrame({'rkey': ['ARG','FRA','VNM'],
-                    'country': ["Argentina", "France", "Vietnam" ],
+                    'country': ["Argentna", "France", "Vietnam" ],
                     'region': ["Latin America", "Europe", "Asia Pacific"]})
 
 print ("df1: \n",df1)
 print ("df2: \n", df2)
 
 #Merge df1 and df2 on the lkey and rkey columns. 
-df_merged = df1.merge(df2, left_on='lkey', right_on='rkey',how="inner")
+df_merged = df1.merge(df2, left_on='lkey', right_on='rkey',how="outer")
 print ("df_merged: \n", df_merged)
+print("Gee whiz, I hope my edits are correct!")
 
 #for more info on merge function see here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.merge.html
