05d91503b89e4a15700d6c7787ce37c2c345a587
AlyPavitt <aly.pavitt@gmail.com>
2020-05-27 11:28:49 +0100

    Update merge_R.R

--- r/merge_R.R
+++ r/merge_R.R
@@ -12,9 +12,10 @@ df1
 df2
 
 #join on the ISO3 columns - here they are called lkey and rkey for clarity
-df_merged<- merge(df1,df2,by.x<-"lkey", by.y ="rkey",all = TRUE)
+df_merged<- merge(df1,df2,by.x<-"lkey", by.y ="rkey",all = FALSE) #only instances where the both match
 
 #check it worked 
 df_merged
 #for more info on merge function see here: https://stat.ethz.ch/R-manual/R-devel/library/base/html/merge.html
+
 
