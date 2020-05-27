33d8c474847208ed041ef4ef98ae425b64b268bd
corinnar <corinna.ravilious@unep-wcmc.org>
2020-05-27 11:24:13 +0100

    Update merge_python.py
    Changed join from inner to outer

--- python/merge_python.py
+++ python/merge_python.py
@@ -15,7 +15,7 @@ print ("df1: \n",df1)
 print ("df2: \n", df2)
 
 #Merge df1 and df2 on the lkey and rkey columns. 
-df_merged = df1.merge(df2, left_on='lkey', right_on='rkey',how="inner")
+df_merged = df1.merge(df2, left_on='lkey', right_on='rkey',how="outer")
 print ("df_merged: \n", df_merged)
 
 #for more info on merge function see here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.merge.html
