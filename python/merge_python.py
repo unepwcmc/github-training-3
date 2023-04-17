5b593631ee29dd2e90dcfccaf62b6b4ebbc542be
bexgottlieb <87428204+bexgottlieb@users.noreply.github.com>
2023-04-17 15:35:24 +0100

    Update merge_python.py
    change join type

--- python/merge_python.py
+++ python/merge_python.py
@@ -1,7 +1,9 @@
 #Example script
 #Aim: merging/joining two dataframes (tables) based on a common column
 
+print('hello world')
 #import pandas library
+
 import pandas as pd
 
 #make dataframes
@@ -15,7 +17,7 @@ print ("df1: \n",df1)
 print ("df2: \n", df2)
 
 #Merge df1 and df2 on the lkey and rkey columns. 
-df_merged = df1.merge(df2, left_on='lkey', right_on='rkey',how="inner")
+df_merged = df1.merge(df2, left_on='lkey', right_on='rkey',how="outer")
 print ("df_merged: \n", df_merged)
 
 #for more info on merge function see here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.merge.html
